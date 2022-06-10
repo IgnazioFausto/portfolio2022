63caa79c95f0f493d4ccc61d7c9d15f563d1f691
IgnazioFausto <nachojam777@gmail.com>
2022-06-10 17:51:12 +0300

    boton listo scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Footer from "./components/footer/Footer";
 import SobreMi from "./components/PagesContainer/Pages/SobreMi/SobreMi";
 import Works from "./components/PagesContainer/Pages/Works/Works";
 import Contacto from "./components/PagesContainer/Pages/Contacto/Contacto";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./components/navbar/NavBar";
-import Portfolio from "./components/portfolio/Portfolio";
 import "./sass/main.scss";
 
 function App() {
@@ -17,10 +15,9 @@ function App() {
           <Route path="/sobremi" element={<SobreMi />} />
           <Route path="/works" element={<Works />} />
           <Route path="/contacto" element={<Contacto />} />
-          <Route path="/portfolio" element={<Portfolio />} />
           <Route path="*" element={<SobreMi />} />
         </Routes>
-        {/* <Footer /> */}
+        
       </BrowserRouter>
     </>
   );
