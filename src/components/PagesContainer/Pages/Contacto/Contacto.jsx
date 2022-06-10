e178dd6f0c9e5da991142e623fe4a4a8539c488d
IgnazioFausto <nachojam777@gmail.com>
2022-06-11 00:16:03 +0300

    nav ok con sass, solo falta works

--- src/components/PagesContainer/Pages/Contacto/Contacto.jsx
+++ src/components/PagesContainer/Pages/Contacto/Contacto.jsx
@@ -1,16 +1,28 @@
-
-
 function Contacto() {
   return (
     <>
       <div className="contenedor">
         <h3 className="title">¡Contáctame!</h3>
         <p className="paragraph">
-          A través de mis redes y medios de comunicación, <br /> <b>espero tu mensaje.</b>
+          A través de mis redes y medios de comunicación, <br />{" "}
+          <b>espero tu mensaje.</b>
         </p>
-        <div className="button">
-          <a href="#">LinkedIn</a>
-
+        <div className="contenedor-botones">
+          <div className="button">
+            <a href="#">LinkedIn</a>
+          </div>
+          <div className="button">
+            <a href="#">Github</a>
+          </div>
+          <div className="button">
+            <a href="#">Facebook</a>
+          </div>
+          <div className="button">
+            <a href="#">SoloLearn</a>
+          </div>
+          <div className="button">
+            <a href="#">Gmail</a>
+          </div>
         </div>
       </div>
     </>
