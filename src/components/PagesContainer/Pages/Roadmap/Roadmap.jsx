6d5bd1174034c5f6fb37868ef3d58c3edddcf9ad
IgnazioFausto <nachojam777@gmail.com>
2022-06-17 17:29:17 +0200

    roadmap listo scss

--- src/components/PagesContainer/Pages/Roadmap/Roadmap.jsx
+++ src/components/PagesContainer/Pages/Roadmap/Roadmap.jsx
@@ -6,6 +6,25 @@ function Roadmap() {
       <div className="contenedor-map">
         <div className="map">
           <p className="skill">HTML</p>
+          <div className="flecha"></div>
+          <div className="flecha"></div>
+          <div className="flecha"></div>
+          <p className="skill">CSS</p>
+          <div className="flecha"></div>
+          <div className="flecha"></div>
+          <div className="flecha"></div>
+          <p className="skill">JavaScript</p>
+          <div className="flecha"></div>
+          <div className="flecha"></div>
+          <div className="flecha"></div>
+          <p className="skill">React JS</p>
+          <div className="flecha"></div>
+          <div className="flecha"></div>
+          <div className="flecha"></div>
+          <p className="skill">Python</p>
+          <div className="flecha"></div>
+          <div className="flecha"></div>
+          <div className="flecha"></div>
         </div>
       </div>
     </div>
