f032f7e69c7f5f0cb6464324578c65a62d591193
IgnazioFausto <nachojam777@gmail.com>
2022-06-18 15:36:25 +0200

    roadmap, skills, scss listos

--- src/components/PagesContainer/Pages/Roadmap/Roadmap.jsx
+++ src/components/PagesContainer/Pages/Roadmap/Roadmap.jsx
@@ -1,30 +1,101 @@
+import { BiTask } from "react-icons/bi";
+
 function Roadmap() {
   return (
     <div className="contenedor">
       <h3 className="title">Roadmap</h3>
-      <p className="paragraph">Conoce mi mapa de aprendizaje hasta ahora.</p>
+      <p className="paragraph"
+      >Conoce mi mapa de aprendizaje hasta ahora.</p>
       <div className="contenedor-map">
         <div className="map">
           <p className="skill">HTML</p>
           <div className="flecha"></div>
           <div className="flecha"></div>
+          <p className="skill">CSS</p>
           <div className="flecha"></div>
-          <p className="skill">CSS</p>  
           <div className="flecha"></div>
+          <p className="skill">Git/Github</p>
+          <div className="flecha"></div>
+          <div className="flecha"></div>
+          <p className="skill">Bootstrap 5.0</p>
           <div className="flecha"></div>
           <div className="flecha"></div>
           <p className="skill">JavaScript</p>
           <div className="flecha"></div>
           <div className="flecha"></div>
+        </div>
+        <div className="map">
+          <p className="skill">JQuery</p>
+          <div className="flecha"></div>
+          <div className="flecha"></div>
+          <p className="skill">SASS</p>
+          <div className="flecha"></div>
           <div className="flecha"></div>
           <p className="skill">React JS</p>
-          <div className="flecha"></div>
           <div className="flecha"></div>
           <div className="flecha"></div>
           <p className="skill">Python</p>
           <div className="flecha"></div>
           <div className="flecha"></div>
+          <p className="skill">Django</p>
           <div className="flecha"></div>
+          <div className="flecha"></div>
+        </div>
+        <div className="map">
+          <p className="skill">React Native</p>
+          <div className="flecha"></div>
+          <div className="flecha"></div>
+          <div className="flecha"></div>
+          <div className="flecha"></div>
+        </div>
+      </div>
+      <div className="contenedor">
+        <p className="title">Que se hacer?</p>
+        <div className="flecha"></div>
+        <p className="paragraph">En que puedo colaborar:</p>
+        <div className="flecha"></div>
+        <div className="colab">
+          <p className="li-element">
+            <BiTask className="icono-lista" />
+            Maquetado de sitios web
+          </p>
+          <p className="li-element">
+            <BiTask className="icono-lista" />
+            Revision de SEO
+          </p>
+          <p className="li-element">
+            <BiTask className="icono-lista" />
+            Estilado con CSS y SASS
+          </p>
+          <p className="li-element">
+            <BiTask className="icono-lista" />
+            Disenio responsive
+          </p>
+          <p className="li-element">
+            <BiTask className="icono-lista" />
+            Desarrollo de aplicaciones web
+          </p>
+          <p className="li-element">
+            <BiTask className="icono-lista" />
+            Integracion con APIS
+          </p>
+          <p className="li-element">
+            <BiTask className="icono-lista" />
+            Trabajo en equipo con Git & Github
+          </p>
+        </div>
+        <div className="flecha"></div>
+        <div className="flecha"></div>
+      </div>
+      <div className="contenedor">
+        <p className="paragraph">Como pienso seguir mi Roadmap:</p>
+        <div className="map">
+          <div className="flecha"></div>
+          <div className="flecha"></div>
+          <div className="flecha"></div>
+          <p className="skill">Mongo DB</p>
+          <p className="skill">Express.js</p>
+          <p className="skill">Node.js</p>
         </div>
       </div>
     </div>
